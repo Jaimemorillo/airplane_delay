6c4e1dc125e78ff4abffa79fa8339c343cab1801
Jaime <jaimorile@hotmail.es>
2021-11-28 22:58:56 +0100

    Managing of dates

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 package es.upm.airplane
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
@@ -15,6 +15,7 @@ object Main extends App{
 
   val sc = spark.sparkContext
 
+  // Create schema and read csv
   val schema = StructType(Array(
     StructField("Year",IntegerType,true),
     StructField("Month",IntegerType,true),
@@ -30,7 +31,7 @@ object Main extends App{
     StructField("ActualElapsedTime",StringType,true),
     StructField("CRSElapsedTime",IntegerType,true),
     StructField("AirTime",StringType,true),
-    StructField("ArrDeplay",IntegerType,true),
+    StructField("ArrDelay",IntegerType,true),
     StructField("DepDelay",IntegerType,true),
     StructField("Origin",StringType,true),
     StructField("Dest",StringType,true),
@@ -49,14 +50,49 @@ object Main extends App{
 
   val df = spark.read.option("header",true).option("delimiter", ",").schema(schema).csv("./data/2008.csv.bz2")
 
-  //println(df.printSchema())
-
+  // Remove forbidden columns and not useful ones
   val dfRemoveColumns= df.drop("ArrTime", "ActualElapsedTime",
     "AirTime", "TaxiIn", "Diverted", "CarrierDelay", "WeatherDelay", "NASDelay",
-    "SecurityDelay", "LateAircraftDelay")
+    "SecurityDelay", "LateAircraftDelay", "CancellationCode")
 
-  //println(dfRemoveColumns.groupBy("Cancelled").count().show())
-  //CancellationCode has a lot of empty values, caused by flights which were not cancelled.
-  println(dfRemoveColumns.groupBy("CancellationCode").count().show())
-  //println(dfRemoveColumns.show(5))
+  //Filter out cancelled flights
+  val dfFilterCancelled = dfRemoveColumns.filter($"Cancelled" === 0)
+    .drop("Cancelled")
+
+  //Merge full date
+  val dfDate = dfFilterCancelled
+    .withColumn("DateString",
+    concat(lpad($"DayofMonth", 2, "0"),
+      lit("-"),
+      lpad($"Month", 2, "0"),
+      lit("-"),
+      $"Year"))
+    .withColumn("Date", to_date($"DateString", "dd-MM-yyyy"))
+    .withColumn("DayofYear", date_format($"Date", "D"))
+    .drop("DayofMonth", "Month", "Year", "DateString")
+
+  //Leap-year¿?
+  println(dfDate.where($"Date"==="2008-02-29").show())
+  //println(dfDate.select(max("Date"),max("DayofYear")).show())
+
+  //Separate times
+  /*def sepTimes(dfIni: DataFrame, columnName:String): DataFrame = {
+    val dfOut = dfIni.withColumn(columnName + "Hour", col(columnName)/100)
+      .withColumn(columnName + "Hour", col(columnName)%100)
+    return dfOut
+  }*/
+
+  //Cyclical features encoding
+  def cyclicalEncodingTime (dfIni: DataFrame, columnName:String) : DataFrame = {
+    // Assign 0 to 00:00 until 1439 to 23:59 (= 24 hours x 60 minutes)
+    //2 * math.pi * df["x"] / 1439
+    val dfOut = dfIni.withColumn("x", col(columnName))
+    return dfOut
+  }
+
+  def cyclicalEncodingDate(dfIni: DataFrame, columnName:String) : DataFrame = {
+    val dfOut = dfIni.withColumn("x", col(columnName))
+    return dfOut
+  }
+
 }
